9a53238b64626259505087cd0d1c294d1165ec18
leonardofrp5 <leonardofrp5@gmail.com>
2020-02-24 07:49:40 -0500

    added new component in register and validations

---
+++ wolox-books/src/app/login/constants.js
@@ -0,0 +1,32 @@
+export const inputLogin = [
+  {
+    key: 'name',
+    text: 'Nombre',
+    type: 'text',
+    formControlName: 'firstName',
+  },
+  {
+    key: 'lastname',
+    text: 'Apellido',
+    type: 'text',
+    formControlName: 'lastName',
+  },
+  {
+    key: 'email',
+    text: 'Correo',
+    type: 'text',
+    formControlName: 'email',
+  },
+  {
+    key: 'password',
+    text: 'Contraseña',
+    type: 'password',
+    formControlName: 'password',
+  },
+  {
+    key: 'passwordConfirmation',
+    text: 'Confirmar contrraseña',
+    type: 'password',
+    formControlName: 'passwordConfirmation',
+  }
+];
