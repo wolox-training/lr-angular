371c72a24ff72e0a6fb25518f2548aadd382f4e8
leonardofrp5 <leonardofrp5@gmail.com>
2020-02-25 10:40:03 -0500

    added router

--- wolox-books/src/app/app-routing.module.ts
+++ wolox-books/src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { RegisterComponent } from  './screens/register/register.component';
+import { LoginComponent } from './screens/login/login.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', 
+    component: AppComponent
+  },  
+  {
+    path: 'sing-up', 
+    component: RegisterComponent
+  },   
+  {
+    path: 'login', 
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
