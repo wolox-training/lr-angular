f2a90ffbedc31208fc28533d994cde3424d133e5
leonardofrp5 <leonardofrp5@gmail.com>
2020-02-27 10:35:36 -0500

    used guards in router

--- wolox-books/src/app/app-routing.module.ts
+++ wolox-books/src/app/app-routing.module.ts
@@ -2,26 +2,36 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from  './screens/register/register.component';
 import { LoginComponent } from './screens/login/login.component';
-import { ListBooksComponent } from './screens/list-books/list-books.component'
+import { ListBooksComponent } from './screens/list-books/list-books.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { UnauthComponent } from './screens/unauth/unauth.component';
+import { AuthGuardGuard } from './guards/auth-guard.guard';
+import { UnauthGuard } from './guards/unauth.guard';
 
 
 const routes: Routes = [ 
   {
     path: '', 
-    redirectTo: 'register', 
+    redirectTo: 'unauth', 
     pathMatch: 'full'
   },
   {
-    path: 'register', 
-    component: RegisterComponent
+    path: '', 
+    component: AuthComponent,
+    canActivate: [AuthGuardGuard],
+    children: [
+      { path: '', redirectTo: 'books', pathMatch: 'full' },
+      { path: 'books', component: ListBooksComponent}
+    ]
   },
   {
-    path: 'login', 
-    component: LoginComponent
-  },
-  {
-    path: 'books', 
-    component: ListBooksComponent
+    path: 'unauth',
+    component: UnauthComponent,
+    canActivate: [UnauthGuard],
+    children: [
+      { path: '', component: LoginComponent},
+      { path: 'register', component: RegisterComponent}
+    ]
   }
 ];
 
---
+++ wolox-books/src/app/guards/auth-guard.guard.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { LoginService } from '../services/login.service';
+import { Router } from "@angular/router";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardGuard implements CanActivate {
+
+  constructor(private loginService: LoginService, private router: Router){ }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      // const verifyToken = this.loginService.getLocalStorage();
+
+      // console.log(next);
+      // console.log(state);
+      // return verifyToken ? true : (this.router.navigateByUrl('/unauth'), false);
+
+
+
+      const currentUser = this.loginService.getLocalStorage();
+
+    if(currentUser){
+      console.log(currentUser);
+      //this.router.navigateByUrl('/books');
+      return true;
+    }
+    else{
+      this.router.navigateByUrl('/unauth');
+      return false;
+    }
+  }
+}
