a6e11645c18513d99e88643e02e02ad95b76e3e4
leonardofrp5 <leonardofrp5@gmail.com>
2020-03-02 15:07:01 -0500

    used ngModelChange

--- wolox-books/src/app/screens/list-books/components/input-search/input-search.component.ts
+++ wolox-books/src/app/screens/list-books/components/input-search/input-search.component.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-input-search',
   templateUrl: './input-search.component.html',
   styleUrls: ['./input-search.component.scss']
 })
+
 export class InputSearchComponent implements OnInit {
+  searchText = '';
+  toggleInputSearch(e) {
+    this.searchText = e;
+    this.query.emit(this.searchText);
+  }
 
   @Output() query = new EventEmitter();
 
   constructor() { }
-
+  
   ngOnInit(): void {
-  }
-
-  somethingChanged(event) {
-    this.query.emit(event.target.value);
   }
 }
